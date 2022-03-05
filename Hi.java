2d04cac0957968489fa27c2a6d920b055df5c3cb
imamuddin gouse <66384320+Gouse143@users.noreply.github.com>
2022-03-05 22:30:41 +0530

    Add files via upload

---
+++ Hi.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hi 
+{
+    public static void main(String[] args)
+    {
+        Scanner scn=new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n= scn.nextInt();
+        
+
+    }
+    
+}
